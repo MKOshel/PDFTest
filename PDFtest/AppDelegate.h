7019e4989644af17cc84bd3969e3f4eae10ffdf6
Dragos <dragos@liveuqas-mbp.liveu.tv>
2015-12-21 11:47:33 +0200

    Initial Commit

---
+++ PDFtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PDFtest
+//
+//  Created by Dragos on 12/21/15.
+//  Copyright © 2015 Tellence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
