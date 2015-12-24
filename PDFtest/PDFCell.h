cf4015704e806113928d5d252603f1be8158fdf1
Dragos <dragos@liveuqas-macbook-pro.local>
2015-12-24 14:42:04 +0200

    more info on Bill

---
+++ PDFtest/PDFCell.h
@@ -0,0 +1,13 @@
+//
+//  PDFCell.h
+//  PDFtest
+//
+//  Created by Dragos on 12/24/15.
+//  Copyright © 2015 Tellence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PDFCell : UITableViewCell
+
+@end
---
+++ PDFtest/PDFCell.m
@@ -0,0 +1,23 @@
+//
+//  PDFCell.m
+//  PDFtest
+//
+//  Created by Dragos on 12/24/15.
+//  Copyright © 2015 Tellence. All rights reserved.
+//
+
+#import "PDFCell.h"
+
+@implementation PDFCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
